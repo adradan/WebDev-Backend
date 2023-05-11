c4414a39e814c363081204b4559f881da6914bc0
adradan <ericvsolcruz@gmail.com>
2023-05-10 20:49:38 -0400

    Fixed FK associations + added employee route

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { Employee } = require("../models");
+const { Employee, Task } = require("../models");
 
 const router = express.Router();
 
@@ -15,5 +15,27 @@ router.get("/", async (req, res) => {
         });
     }
 });
+
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const employee = await Employee.findAll({
+            where: {
+                id,
+            },
+            include: [{
+                model: Task,
+                as: 'tasks',
+            }],
+        });
+        res.json(employee);
+    } catch (err) {
+        console.error(`ERROR: GET /employees/${id}`);
+        console.error(err);
+        res.status(500).json({
+            message: `Error grabbing employee ID ${id}`,
+        });
+    }
+})
 
 module.exports = router;
