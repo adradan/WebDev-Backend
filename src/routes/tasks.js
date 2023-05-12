039eba235f1c95c7743455877784301657eb5706
Hans Chaudry <hanschaudry@outlook.com>
2023-05-12 07:51:58 -0400

    Implemented route to edit a task based on id

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require("express");
 var bodyParser = require('body-parser')
-const { Task } = require("../models");
+const { Task, Employee } = require("../models");
 
 const router = express.Router();
 const jsonParser = bodyParser.json()
@@ -20,8 +20,8 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
     try {
-        const userID = req.params.id;
-        const task = await Task.findOne({where:{id:userID}});
+        const taskID = req.params.id;
+        const task = await Task.findOne({where:{id:taskID}});
         res.json(task);
     } catch (err) {
         console.error("ERROR: GET /task/");
@@ -33,13 +33,36 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/add", jsonParser,  (req, res) => {
-    const data = req.body;
-    let newTask = Task.build(data);
-    newTask.save();
-    console.log(newTask.id);
-    return res.json({msg: "new task added"});
+    try{
+        const data = req.body;
+        let newTask = Task.build(data);
+        newTask.save();
+        console.log(newTask.id);
+        return res.json({msg: "new task added"});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({
+            message: "Could not add task",
+        });
+    }
 });
 
 //todo: Make route to update/edit task
+router.put("/edit/:id", jsonParser, (req, res) =>{
+    const taskID = req.params.id;
+    const updates = req.body;
+
+    try{
+        Task.update(updates, {where:{id:taskID}});
+        res.send("updated the task");
+        return; 
+    }catch(err){
+        console.error(err);
+        res.status(500).json({
+            message: "Error could not update task with given id",
+        });
+    }
+});
+
 //todo: Make route to delete task
 module.exports = router;
